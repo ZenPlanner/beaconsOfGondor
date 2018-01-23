de0f61f96eca0b805fa730f3d05ece307fefec5f
Mike Darschewski <mike@zenplanner.com>
2018-01-23 06:20:07 -0700

    Created LEDController

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ var os = require('os');
 var interfaces = os.networkInterfaces( );
 var hostname = os.hostname();
 var Gpio = require('pigpio').Gpio;
+var ledController = require('./ledController.js');
 // setup the io pins
 var pins = [
         new Gpio(23, {mode: Gpio.OUTPUT}), /* Red */
@@ -16,176 +17,15 @@ var pins = [
         new Gpio(14, {mode: Gpio.OUTPUT})  /* White */
     ];
 
-//set some globals
-var current = {
-	fader: {
-		r:0,
-		g:0,
-		b:0,
-		w:0,
-        rRatio: 1,
-        gRatio: 1,
-        bRatio: 1,
-        wRatio: 1
-	},
-    r: 0,
-    g: 0,
-    b: 0,
-    w: 0,
-    pattern: 'normal',
-    frequency: 100,
-    state: 'on'
-};
-
-// Converts a 6-8 digit hex string to rgbw colors
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-        w: (result[4] != null) ? parseInt(result[4], 16) : 0
-    } : null;
-}
-
-//Set the pwm hardware pins to the color values
-function setColorPins(r, g, b, w) {
-    if (r < 0) r = 0;
-    if (g < 0) g = 0;
-    if (b < 0) b = 0;
-    if (w < 0) w = 0;
-    pins[0].pwmWrite(Math.floor(r));
-    pins[1].pwmWrite(Math.floor(g));
-    pins[2].pwmWrite(Math.floor(b));
-    pins[3].pwmWrite(Math.floor(w));   
-}
-
-// pattern: normal, strobe, fade, stripe
-function setColor(hexColor) {
-    var colorMap = hexToRgb(hexColor);
-    if (colorMap != null) {
-        setColorPins(colorMap.r, colorMap.g, colorMap.b, colorMap.w);
-    }
-}
-
-// set the colors on a new call
-function setCurrentColors(hexColor) {
-	var colorMap = hexToRgb(hexColor);
-    current.r = colorMap.r;
-    current.g = colorMap.g;
-    current.b = colorMap.b;
-    current.w = colorMap.w;
-    current.fader.r = colorMap.r;
-    current.fader.g = colorMap.g;
-    current.fader.b = colorMap.b;
-    current.fader.w = colorMap.w;
-    current.fader.rRatio = colorMap.r / 255;
-    current.fader.gRatio = colorMap.g / 255;
-    current.fader.bRatio = colorMap.b / 255;
-    current.fader.wRatio = colorMap.w / 255;
-}
-
-function fadeIn() {
-	if (current.r < current.fader.r)
-		current.r = current.r + (current.fader.rRatio);
-	if (current.g < current.fader.g)
-		current.g = current.g + (current.fader.gRatio);
-	if (current.b < current.fader.b)
-		current.b = current.b + (current.fader.bRatio);
-	if (current.w < current.fader.w)
-		current.w = current.w + (current.fader.wRatio);
-	setColorPins(current.r, current.g, current.b, current.w);
-	if (current.r >= current.fader.r &&
-		current.g >= current.fader.g &&
-		current.b >= current.fader.b &&
-		current.w >= current.fader.w) {
-	   current.state = 'off'; // start fading out
-    }
-}
-
-function fadeOut() {
-	if (current.r > 0)
-		current.r = current.r - (current.fader.rRatio);
-	if (current.g > 0)
-		current.g = current.g - (current.fader.gRatio);
-	if (current.b > 0)
-		current.b = current.b - (current.fader.bRatio);
-	if (current.w > 0)
-		current.w = current.w - (current.fader.wRatio);
-	setColorPins(current.r, current.g, current.b, current.w);
-	if (current.r <= 0 && current.g <= 0 &&
-            current.b <= 0 && current.w <= 0) 
-		current.state = 'on'; // start fading in
-}
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+function setHardwarePins(current) {
+    pins[0].pwmWrite(Math.floor(current.r));
+    pins[1].pwmWrite(Math.floor(current.g));
+    pins[2].pwmWrite(Math.floor(current.b));
+    pins[3].pwmWrite(Math.floor(current.w));
 }
 
 function loop() {
-    switch (current.pattern) {
-        case 'normal':
-            setColorPins(current.r, current.g, current.b, current.w);
-            break;
-        case 'strobe':
-            if (current.state === 'on') {
-                setColorPins(0, 0, 0, 0);
-                current.state = 'off';
-            } else {
-                setColorPins(current.r, current.g, current.b, current.w);
-                current.state = 'on';
-            }
-            break;
-        case 'outage':
-            if (current.state == 'red') {
-                setColorPins(255,60,0,0);
-                current.state = 'yellow';
-            } else {
-                setColorPins(255,0,0,0);
-                current.state = 'red';
-            }
-            break;
-        case 'fade':
-            if (current.state === 'on') {
-                fadeIn();
-            } else {
-                fadeOut();
-            }
-            break;
-        case 'christmas':
-            if (current.state == 'green') {
-                setColorPins(getRandomInt(0,255),0,0,0);
-                current.state = 'red';
-            } else {
-                setColorPins(0,getRandomInt(0,255),0,0);
-                current.state = 'green';
-            }
-            break;
-        case 'party':
-            setColorPins(getRandomInt(0, current.r), getRandomInt(0, current.g), getRandomInt(0, current.b), getRandomInt(0, current.w));
-            break;
-        case 'randomfade':
-            if (current.r === current.g === current.b === current.w === 0) {
-                setColorPins(getRandomInt(0, current.fader.r), getRandomInt(0, current.fader.g), getRandomInt(0, current.fader.b), getRandomInt(0, current.fader.w));
-            }
-            if (current.state === 'on') {
-                fadeIn();
-            } else {
-                fadeOut();
-            }
-            break;
-        case 'random':
-            if (current.state === 'on') {
-                setColorPins(0, 0, 0, 0);
-                current.state = 'off';
-            } else {
-                setColorPins(getRandomInt(0, current.r), getRandomInt(0, current.g), getRandomInt(0, current.b), getRandomInt(0, current.w));
-                current.state = 'on';
-            }
-            break;
-    }
+    setHardwarePins(ledController.getPinSettings());
 }
 
 function getLocalIpAddresses() {
@@ -202,16 +42,16 @@ function getLocalIpAddresses() {
 }
 
 function printState() {
-    var logString = "Fader Setting: r:"+current.fader.r+" g:"+current.fader.g+
-                    " b:"+current.fader.b+" w:"+current.fader.w+"\r\n"+
-                    "Current Settings: r:"+current.r+" g:"+current.g+
-                    " b:"+current.b+" w:"+current.w+" pattern:"+
-                    current.pattern+" frequency:"+current.frequency+
-                    " state:"+current.state
-		    + " \r\nrRatio: " +current.fader.rRatio + "\r\n"+
-		     " \r\ngRatio: " +current.fader.gRatio + "\r\n"+
-		     " \r\nbRatio: " +current.fader.bRatio + "\r\n"+
-		     " \r\nwRatio: " +current.fader.wRatio + "\r\n";
+    var logString = "Fader Setting: r:"+ledController.current.fader.r+" g:"+ledController.current.fader.g+
+                    " b:"+ledController.current.fader.b+" w:"+ledController.current.fader.w+"\r\n"+
+                    "Current Settings: r:"+ledController.current.r+" g:"+ledController.current.g+
+                    " b:"+ledController.current.b+" w:"+ledController.current.w+" pattern:"+
+                    ledController.current.pattern+" frequency:"+ledController.current.frequency+
+                    " state:"+ledController.current.state
+		    + " \r\nrRatio: " +ledController.current.fader.rRatio + "\r\n"+
+		     " \r\ngRatio: " +ledController.current.fader.gRatio + "\r\n"+
+		     " \r\nbRatio: " +ledController.current.fader.bRatio + "\r\n"+
+		     " \r\nwRatio: " +ledController.current.fader.wRatio + "\r\n";
     console.log(logString);
 }
 
@@ -230,9 +70,9 @@ socket.on('receivedColor', function (data) {
             clearTimeout(currentTimeout);
             var destination = data.destination != null ? data.destination : 'ALL';
             if(destination == 'ALL' || hostname.indexOf(destination) > -1) {
-                setCurrentColors(data.color);
-                current.pattern = data.pattern;
-                current.frequency = data.frequency;
+                ledController.setCurrentColors(data.color);
+                ledController.current.pattern = data.pattern;
+                ledController.current.frequency = data.frequency;
                 currentTimeout = setInterval(loop, data.frequency);
             }
 }) ;
---
+++ public/ledController.js
@@ -0,0 +1,199 @@
+class LEDController {
+
+    current = {
+        fader: {
+            r:0,
+            g:0,
+            b:0,
+            w:0,
+            rRatio: 1,
+            gRatio: 1,
+            bRatio: 1,
+            wRatio: 1
+        },
+        r: 0,
+        g: 0,
+        b: 0,
+        w: 0,
+        pattern: 'normal',
+        frequency: 100,
+        state: 'on'
+    };
+
+    constructor () {
+
+    };
+
+    // Converts a 6-8 digit hex string to rgbw colors
+    static hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16),
+            w: (result[4] != null) ? parseInt(result[4], 16) : 0
+        } : null;
+    };
+
+    //Gets a random int between [min, max). The minimum is inclusive and the maximum is exclusive
+    static getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    // set the colors on a new call
+    setCurrentColors(hexColor) {
+        var colorMap = this.hexToRgb(hexColor);
+        this.current.r = colorMap.r;
+        this.current.g = colorMap.g;
+        this.current.b = colorMap.b;
+        this.current.w = colorMap.w;
+        this.current.fader.r = colorMap.r;
+        this.current.fader.g = colorMap.g;
+        this.current.fader.b = colorMap.b;
+        this.current.fader.w = colorMap.w;
+        this.current.fader.rRatio = colorMap.r / 255;
+        this.current.fader.gRatio = colorMap.g / 255;
+        this.current.fader.bRatio = colorMap.b / 255;
+        this.current.fader.wRatio = colorMap.w / 255;
+        return this.current;
+    };
+
+    fadeIn() {
+        if (this.current.r < this.current.fader.r)
+            this.current.r = this.current.r + (this.current.fader.rRatio);
+        if (this.current.g < this.current.fader.g)
+            this.current.g = this.current.g + (this.current.fader.gRatio);
+        if (this.current.b < this.current.fader.b)
+            this.current.b = this.current.b + (this.current.fader.bRatio);
+        if (this.current.w < this.current.fader.w)
+            this.current.w = this.current.w + (this.current.fader.wRatio);
+        if (this.current.r >= this.current.fader.r &&
+            this.current.g >= this.current.fader.g &&
+            this.current.b >= this.current.fader.b &&
+            this.current.w >= this.current.fader.w) {
+            this.current.state = 'off'; // start fading out
+        }
+        return this.current;
+    };
+
+    fadeOut() {
+        if (this.current.r > 0)
+            this.current.r = this.current.r - (this.current.fader.rRatio);
+        if (this.current.g > 0)
+            this.current.g = this.current.g - (this.current.fader.gRatio);
+        if (this.current.b > 0)
+            this.current.b = this.current.b - (this.current.fader.bRatio);
+        if (this.current.w > 0)
+            this.current.w = this.current.w - (this.current.fader.wRatio);
+        if (this.current.r <= 0 && this.current.g <= 0 &&
+            this.current.b <= 0 && this.current.w <= 0)
+            this.current.state = 'on'; // start fading in
+        return this.current;
+    };
+
+    setReturnValue(r,g,b,w,state) {
+        this.current.state = state;
+        var retVal = {};
+        retVal.r = r;
+        retVal.g = g;
+        retVal.b = b;
+        retVal.w = w;
+        return retVal;
+    };
+
+    goNormal() {
+        return this.setReturnValue(this.current.r, this.current.g, this.current.b, this.current.w, 'normal');
+    }
+
+    goStrobe() {
+        if (this.current.state === 'on') {
+            return this.setReturnValue(0, 0, 0, 0, 'off');
+        } else {
+            return this.setReturnValue(this.current.r, this.current.g, this.current.b, this.current.w, 'on');
+        }
+    }
+
+    goOutage() {
+        if (this.current.state == 'red') {
+            return this.setReturnValue(255, 60, 0, 0, 'yellow');
+        } else {
+            return this.setReturnValue(255, 0, 0, 0, 'red');
+        }
+    }
+
+    goFade() {
+        if (this.current.state === 'on') {
+            return this.fadeIn();
+        } else {
+            return this.fadeOut();
+        }
+
+    }
+
+    goChristmas() {
+        if (this.current.state == 'green') {
+            return this.setReturnValue(this.getRandomInt(0, 255), 0, 0, 0, 'red');
+        } else {
+            return this.setReturnValue(0, this.getRandomInt(0, 255), 0, 0, 'green');
+        }
+    }
+
+    goParty() {
+        return this.setReturnValue(this.getRandomInt(0, this.current.r), this.getRandomInt(0, this.current.g), this.getRandomInt(0, this.current.b), this.getRandomInt(0, this.current.w), 'party');
+    }
+
+    goRandom() {
+        if (this.current.state === 'on') {
+            return this.setReturnValue(0, 0, 0, 0, 'off');
+        } else {
+            return this.setReturnValue(this.getRandomInt(0, this.current.r), this.getRandomInt(0, this.current.g), this.getRandomInt(0, this.current.b), this.getRandomInt(0, this.current.w), 'on');
+        }
+    }
+
+    goRandomFade() {
+        if (this.current.r === this.current.g === this.current.b === this.current.w === 0) {
+            return this.setReturnValue(this.getRandomInt(0, this.current.fader.r), this.getRandomInt(0, this.current.fader.g), this.getRandomInt(0, this.current.fader.b), this.getRandomInt(0, this.current.fader.w));
+        }
+        if (this.current.state === 'on') {
+            return this.fadeIn();
+        } else {
+            return this.fadeOut();
+        }
+
+    }
+
+    getPinSettings() {
+        var retVal = {};
+        switch (this.current.pattern) {
+            case 'normal':
+                retVal = this.goNormal();
+                break;
+            case 'strobe':
+                retVal = this.goStrobe();
+                break;
+            case 'outage':
+                retVal = this.goOutage();
+                break;
+            case 'fade':
+                retVal = this.goFade();
+                break;
+            case 'christmas':
+                retVal = this.goChristmas();
+                break;
+            case 'party':
+                retVal = this.goParty();
+                break;
+            case 'randomfade':
+                retVal = this.goRandomFade();
+                break;
+            case 'random':
+                retVal = this.goRandom();
+                break;
+        }
+        return retVal;
+    };
+};
+
+module.exports = LEDController;
